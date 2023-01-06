415b66444aa8a3a81df41bb22f99e44a6dd95b70
tuansdf <103062682+tuansdf@users.noreply.github.com>
2023-01-06 12:26:04 +0700

    control robot speed

--- src/app.svelte
+++ src/app.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
   import clsx from "clsx";
+  import {
+    getDirectionDiff,
+    getOppositeDirection,
+  } from "/src/blind-path-finding/helpers";
 
-  import Maze, { Cell } from "/src/blind-path-finding/maze";
+  import Maze from "/src/blind-path-finding/maze";
   import Robot from "/src/blind-path-finding/robot";
+  import { Cell, Direction } from "/src/blind-path-finding/types";
   import Button from "/src/components/button.svelte";
+  import Map from "/src/components/map.svelte";
 
   const MAX_ROWS = 30;
   const MAX_COLS = 60;
   const MIN_ROWS = 5;
   const MIN_COLS = 5;
+
+  let wait = 10;
 
   let mapRows = 30;
   let mapCols = 60;
@@ -16,20 +24,23 @@
   let robotRow = 1;
   let robotCol = 1;
 
-  let robotExist = false;
-  let gateExist = false;
+  let robotExist = true;
 
   let paintType: Cell = Cell.WALL;
 
   let started = false;
 
-  let maze: Maze;
+  let currentDirection: Direction;
 
-  $: console.log(mapRows);
+  let map: Cell[][];
 
-  $: map = (() => {
-    const rows = mapRows || MIN_ROWS;
-    const cols = mapCols || MIN_COLS;
+  let steps: number = 0;
+
+  $: resetMap(mapRows, mapCols);
+
+  const resetMap = (createRows: number, createCols: number) => {
+    const rows = createRows || MIN_ROWS;
+    const cols = createCols || MIN_COLS;
 
     const tmp: Cell[][] = new Array(rows);
 
@@ -43,11 +54,19 @@
       tmp[i][cols - 1] = Cell.WALL;
     }
 
-    return tmp;
-  })();
+    tmp[robotRow][robotCol] = Cell.ROBOT;
 
-  const updateMap = (row: number, col: number) => {
-    if (row === 0 || row === mapRows - 1 || col === 0 || col === mapCols - 1)
+    map = tmp;
+  };
+
+  const paintMap = (row: number, col: number) => {
+    if (
+      row === 0 ||
+      row === mapRows - 1 ||
+      col === 0 ||
+      col === mapCols - 1 ||
+      started
+    )
       return;
 
     let toPaint: Cell = Cell.WALL;
@@ -55,9 +74,6 @@
 
     if (currentCell === Cell.ROBOT) {
       robotExist = false;
-    }
-    if (currentCell === Cell.GATE) {
-      gateExist = false;
     }
 
     if (paintType === Cell.ROBOT) {
@@ -74,9 +90,7 @@
       if (currentCell === Cell.GATE) {
         toPaint = Cell.SPACE;
       } else {
-        if (gateExist) return;
         toPaint = Cell.GATE;
-        gateExist = true;
       }
     } else if (paintType === Cell.WALL) {
       if (currentCell === Cell.WALL) {
@@ -89,46 +103,73 @@
     map[row][col] = toPaint;
   };
 
+  const delay = (ms: number) => {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  };
+
   const start = () => {
     if (started) {
       started = false;
+      resetMap(mapRows, mapCols);
     } else {
       started = true;
-      maze = new Maze(map, robotRow, robotCol);
-      new Robot(maze).navigate();
+      const maze = new Maze(map, robotRow, robotCol);
+      new Robot(maze).navigate(async (direction: Direction, cell: Cell) => {
+        if (currentDirection === getOppositeDirection(direction)) {
+          map[robotRow][robotCol] = cell;
+        }
+        currentDirection = direction;
+        const [row, col] = getDirectionDiff(direction);
+        robotCol += col;
+        robotRow += row;
+        map[robotRow][robotCol] = cell;
+        steps++;
+        await delay(wait);
+      });
     }
   };
 </script>
 
-<main
-  class="min-h-screen flex flex-col bg-neutral-100 justify-center items-center"
->
-  <div class="flex gap-8 fixed top-8 items-center mb-4">
-    <div class="flex gap-4">
+<main class="min-h-screen flex gap-8 bg-neutral-100 justify-start items-center">
+  <div class="gap-8 items-center mb-4 ml-16">
+    <div class="">
+      Steps: {steps}
       <div>
         <label for="rows">Rows (max: {MAX_ROWS})</label>
+        <br />
         <input
           type="number"
           id="rows"
           max={MAX_ROWS}
           min={MIN_COLS}
-          class="w-32 p-1.5 border-rose-500 border-2 rounde"
+          class="w-32 p-1.5 border-rose-500 border-2 rounded"
           bind:value={mapRows}
         />
       </div>
       <div>
         <label for="cols">Cols: (max: {MAX_COLS})</label>
+        <br />
         <input
           type="number"
           id="cols"
           max={MAX_COLS}
           min={MIN_COLS}
-          class="w-32 p-1.5 border-rose-500 border-2 rounde"
+          class="w-32 p-1.5 border-rose-500 border-2 rounded"
           bind:value={mapCols}
         />
       </div>
+      <div>
+        <label for="cols">Time</label>
+        <br />
+        <input
+          type="number"
+          id="cols"
+          class="w-32 p-1.5 border-rose-500 border-2 rounded"
+          bind:value={wait}
+        />
+      </div>
     </div>
-    <div class="flex gap-2">
+    <div class="flex flex-col gap-2">
       <Button on:click={() => (paintType = Cell.WALL)}>Wall</Button>
       <Button on:click={() => (paintType = Cell.GATE)}>Gate</Button>
       <Button on:click={() => (paintType = Cell.ROBOT)}>Robot</Button>
@@ -150,27 +191,5 @@
     <Button on:click={start}>{started ? "Reset" : "Start"}</Button>
   </div>
 
-  <div class="grid gap-[1px] mt-16">
-    {#each started ? maze.mapPublic : map as row, i}
-      <div class="flex border-collapse gap-[1px]">
-        {#each row as cell, j}
-          <div
-            on:keypress={() => {}}
-            on:click={() => updateMap(i, j)}
-            class={clsx(
-              {
-                "bg-red-300": cell === Cell.WALL,
-                "bg-blue-300": cell === Cell.GATE,
-                "bg-green-300": cell === Cell.PATH,
-                "bg-green-400": cell === Cell.ROBOT,
-                "hover:bg-neutral-200": cell === Cell.SPACE,
-                "hover:opacity-90": cell !== Cell.SPACE,
-              },
-              "border rounded-sm border-black cursor-pointer w-6 h-6"
-            )}
-          />
-        {/each}
-      </div>
-    {/each}
-  </div>
+  <Map {map} {paintMap} />
 </main>
---
+++ src/components/map.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import clsx from "clsx";
+  import { Cell } from "/src/blind-path-finding/types";
+
+  export let paintMap: (row: number, col: number) => void;
+  export let map: Cell[][];
+</script>
+
+<div class="grid gap-[1px] mt-16">
+  {#each map as row, i}
+    <div class="flex border-collapse gap-[1px]">
+      {#each row as cell, j}
+        <div
+          on:keypress={() => {}}
+          on:click={() => paintMap(i, j)}
+          class={clsx(
+            {
+              "bg-red-300": cell === Cell.WALL,
+              "bg-blue-300": cell === Cell.GATE,
+              "bg-green-200": cell === Cell.PATH,
+              "bg-green-400": cell === Cell.ROBOT,
+              "bg-yellow-300": cell === Cell.CROSS,
+              "hover:bg-neutral-200": cell === Cell.SPACE,
+              "hover:opacity-90": cell !== Cell.SPACE,
+            },
+            "border rounded-sm border-black cursor-pointer w-6 h-6"
+          )}
+        />
+      {/each}
+    </div>
+  {/each}
+</div>
